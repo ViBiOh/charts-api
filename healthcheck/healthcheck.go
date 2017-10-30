8289350ed6f9d2c4373076e936655075b2f4c9f5
Vincent Boutour <vboutour@meilleursagents.com>
2017-10-30 13:36:29 +0100

    Fixing healthcheck

--- healthcheck/healthcheck.go
+++ healthcheck/healthcheck.go
@@ -1,24 +1,42 @@
 package healthcheck
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/ViBiOh/httputils/writer"
 )
 
-const healthcheckPath = `/health`
-
 var handlersToCheck []http.Handler
+var healthRequest *http.Request
 
 // Init charts handler
-func Init(handlers []http.Handler) error {
+func Init(handlers []http.Handler) (err error) {
 	handlersToCheck = handlers
 
-	return nil
+	healthRequest, err = http.NewRequest(http.MethodGet, `/health`, nil)
+	if err != nil {
+		err = fmt.Errorf(`Error while creating health request: %v`, err)
+	}
+
+	return
 }
 
 // Handler for Health request. Should be use with net/http
 func Handler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
+			for _, handler := range handlersToCheck {
+				fakeWriter := writer.ResponseWriter{}
+
+				handler.ServeHTTP(&fakeWriter, healthRequest)
+
+				if status := fakeWriter.Status(); status != http.StatusOK {
+					w.WriteHeader(status)
+					return
+				}
+			}
+
 			w.WriteHeader(http.StatusOK)
 		} else {
 			w.WriteHeader(http.StatusMethodNotAllowed)
