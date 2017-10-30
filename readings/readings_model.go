2426afab7a8d7e6e78d7f2084790be938f622981
Vincent Boutour <bob@vibioh.fr>
2017-10-30 22:09:57 +0100

    Adding basic model for readings

---
+++ readings/readings_model.go
@@ -0,0 +1,18 @@
+package readings
+
+import "github.com/ViBiOh/auth/auth"
+
+type tag struct {
+	id   int64
+	Name string     `json:"name"`
+	User *auth.User `json:"user"`
+}
+
+type reading struct {
+	id     int64
+	URL    string     `json:"url"`
+	Public bool       `json:"public"`
+	Read   bool       `json:"read"`
+	User   *auth.User `json:"user"`
+	Tags   []*tag     `json:"tags"`
+}
