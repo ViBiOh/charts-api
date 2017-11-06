a5c050ffcb7b8d0d39fb2031bebc7cdbf7b83049
Vincent Boutour <bob@vibioh.fr>
2017-11-06 08:46:21 +0100

    Harmonizing error code and code syntax

--- readings/tags_db.go
+++ readings/tags_db.go
@@ -8,17 +8,7 @@ import (
 	"github.com/ViBiOh/httputils/db"
 )
 
-const listTagsOfUserQuery = `
-SELECT
-  id,
-  name
-FROM
-  tags
-WHERE
-  user_id = $1
-`
-
-const listTagsByidsQuery = `
+const findTagsByidsQuery = `
 SELECT
   id,
   name
@@ -26,16 +16,6 @@ FROM
   tags
 WHERE
   id IN ($1)
-`
-
-const listReadingsTagsOfReadingsQuery = `
-SELECT
-  readings_id,
-  tags_id
-FROM
-  readings_tags
-WHERE
-  readings_id IN ($1)
 `
 
 const searchTagsWhereQuery = `
@@ -109,6 +89,16 @@ WHERE
   id = $1
 `
 
+const listReadingsTagsOfReadingsQuery = `
+SELECT
+  readings_id,
+  tags_id
+FROM
+  readings_tags
+WHERE
+  readings_id IN ($1)
+`
+
 func scanTags(rows *sql.Rows, pageSize int64) ([]*tag, error) {
 	var (
 		id   int64
@@ -128,107 +118,8 @@ func scanTags(rows *sql.Rows, pageSize int64) ([]*tag, error) {
 	return list, nil
 }
 
-func scanReadingsTagsForTag(rows *sql.Rows) (map[int64][]int64, error) {
-	var (
-		readingID int64
-		tagID     int64
-	)
-
-	list := make(map[int64][]int64, 0)
-
-	for rows.Next() {
-		if err := rows.Scan(&readingID, &tagID); err != nil {
-			return nil, fmt.Errorf(`Error while scanning reading-tag line: %v`, err)
-		}
-
-		if _, ok := list[tagID]; ok {
-			list[tagID] = append(list[tagID], readingID)
-		} else {
-			list[tagID] = []int64{readingID}
-		}
-	}
-
-	return list, nil
-}
-
-func listTagsByIds(ids []int64) ([]*tag, error) {
-	rows, err := readingsDB.Query(listTagsByidsQuery, db.WhereInInt64(ids))
-	if err != nil {
-		return nil, fmt.Errorf(`Error while listing tags by ids: %v`, err)
-	}
-
-	defer func() {
-		err = db.RowsClose(`listing tags by ids`, rows, err)
-	}()
-
-	return scanTags(rows, 0)
-}
-
-func addTagsForReadings(readings []*reading) error {
-	if len(readings) == 0 {
-		return nil
-	}
-
-	ids := make([]int64, 0)
-	for _, reading := range readings {
-		ids = append(ids, reading.ID)
-	}
-
-	rows, err := readingsDB.Query(listReadingsTagsOfReadingsQuery, db.WhereInInt64(ids))
-	if err != nil {
-		return fmt.Errorf(`Error while listing reading-tag of readings: %v`, err)
-	}
-
-	defer func() {
-		err = db.RowsClose(`listing reading-tag of readings`, rows, err)
-	}()
-
-	tagLinks, err := scanReadingsTagsForTag(rows)
-	if err != nil {
-		return fmt.Errorf(`Error while scanning reading-tag of readings: %v`, err)
-	} else if len(tagLinks) == 0 {
-		return nil
-	}
-
-	tagsIds := make([]int64, 0)
-	tagsByReading := make(map[int64][]int64, 0)
-	for tagID, readingsIds := range tagLinks {
-		tagsIds = append(tagsIds, tagID)
-
-		for _, readingID := range readingsIds {
-			if _, ok := tagsByReading[readingID]; ok {
-				tagsByReading[readingID] = append(tagsByReading[readingID], tagID)
-			} else {
-				tagsByReading[readingID] = []int64{tagID}
-			}
-		}
-	}
-
-	tags, err := listTagsByIds(tagsIds)
-	if err != nil {
-		return fmt.Errorf(`Error while tags for readings: %v`, err)
-	}
-
-	tagsByID := make(map[int64]*tag, 0)
-	for _, tag := range tags {
-		tagsByID[tag.ID] = tag
-	}
-
-	for _, reading := range readings {
-		for _, tagID := range tagsByReading[reading.ID] {
-			if reading.Tags == nil {
-				reading.Tags = make([]*tag, 0)
-			}
-
-			reading.Tags = append(reading.Tags, tagsByID[tagID])
-		}
-	}
-
-	return nil
-}
-
 func searchTags(page, pageSize int64, sortKey string, sortAsc bool, user *auth.User, search string) ([]*tag, error) {
-	if user == nil {
+	if user == nil || user.ID == 0 {
 		return nil, fmt.Errorf(`Unable to search tags of nil User`)
 	}
 
@@ -258,14 +149,14 @@ func searchTags(page, pageSize int64, sortKey string, sortAsc bool, user *auth.U
 	}
 
 	defer func() {
-		err = db.RowsClose(`search tag`, rows, err)
+		err = db.RowsClose(`searching tags`, rows, err)
 	}()
 
 	return scanTags(rows, pageSize)
 }
 
 func countTags(user *auth.User, search string) (count int64, err error) {
-	if user == nil {
+	if user == nil || user.ID == 0 {
 		return 0, fmt.Errorf(`Unable to count tags of nil User`)
 	}
 
@@ -287,6 +178,19 @@ func countTags(user *auth.User, search string) (count int64, err error) {
 	}
 
 	return
+}
+
+func findTagsByIds(ids []int64) ([]*tag, error) {
+	rows, err := readingsDB.Query(findTagsByidsQuery, db.WhereInInt64(ids))
+	if err != nil {
+		return nil, fmt.Errorf(`Error while finding tags by ids: %v`, err)
+	}
+
+	defer func() {
+		err = db.RowsClose(`finding tags by ids`, rows, err)
+	}()
+
+	return scanTags(rows, int64(len(ids)))
 }
 
 func getTag(id int64, user *auth.User) (*tag, error) {
@@ -331,13 +235,13 @@ func saveTag(o *tag, tx *sql.Tx) (err error) {
 
 	if o.ID != 0 {
 		if _, err = usedTx.Exec(updateTagQuery, o.ID, o.Name); err != nil {
-			err = fmt.Errorf(`Error while updating tag for user=%s: %v`, o.user.Username, err)
+			err = fmt.Errorf(`Error while updating tag: %v`, err)
 		}
 	} else {
 		var newID int64
 
 		if err = usedTx.QueryRow(insertTagQuery, o.user.ID, o.Name).Scan(&newID); err != nil {
-			err = fmt.Errorf(`Error while creating tag for user=%s: %v`, o.user.Username, err)
+			err = fmt.Errorf(`Error while creating tag: %v`, err)
 		} else {
 			o.ID = newID
 		}
@@ -367,8 +271,94 @@ func deleteTag(o *tag, tx *sql.Tx) (err error) {
 	}
 
 	if _, err = usedTx.Exec(deleteTagQuery, o.ID); err != nil {
-		err = fmt.Errorf(`Error while deleting tag with ID=%d: %v`, o.ID, err)
+		err = fmt.Errorf(`Error while deleting tag: %v`, err)
 	}
 
 	return
+}
+
+func scanReadingsTagsForTag(rows *sql.Rows) (map[int64][]int64, error) {
+	var (
+		readingID int64
+		tagID     int64
+	)
+
+	list := make(map[int64][]int64, 0)
+
+	for rows.Next() {
+		if err := rows.Scan(&readingID, &tagID); err != nil {
+			return nil, fmt.Errorf(`Error while scanning reading-tag line: %v`, err)
+		}
+
+		if _, ok := list[tagID]; ok {
+			list[tagID] = append(list[tagID], readingID)
+		} else {
+			list[tagID] = []int64{readingID}
+		}
+	}
+
+	return list, nil
+}
+
+func addTagsForReadings(readings []*reading) error {
+	if len(readings) == 0 {
+		return nil
+	}
+
+	ids := make([]int64, 0)
+	for _, reading := range readings {
+		ids = append(ids, reading.ID)
+	}
+
+	rows, err := readingsDB.Query(listReadingsTagsOfReadingsQuery, db.WhereInInt64(ids))
+	if err != nil {
+		return fmt.Errorf(`Error while listing reading-tag of readings: %v`, err)
+	}
+
+	defer func() {
+		err = db.RowsClose(`listing reading-tag of readings`, rows, err)
+	}()
+
+	tagLinks, err := scanReadingsTagsForTag(rows)
+	if err != nil {
+		return fmt.Errorf(`Error while scanning reading-tag of readings: %v`, err)
+	} else if len(tagLinks) == 0 {
+		return nil
+	}
+
+	tagsIds := make([]int64, 0)
+	tagsByReading := make(map[int64][]int64, 0)
+	for tagID, readingsIds := range tagLinks {
+		tagsIds = append(tagsIds, tagID)
+
+		for _, readingID := range readingsIds {
+			if _, ok := tagsByReading[readingID]; ok {
+				tagsByReading[readingID] = append(tagsByReading[readingID], tagID)
+			} else {
+				tagsByReading[readingID] = []int64{tagID}
+			}
+		}
+	}
+
+	tags, err := findTagsByIds(tagsIds)
+	if err != nil {
+		return fmt.Errorf(`Error while tags for readings: %v`, err)
+	}
+
+	tagsByID := make(map[int64]*tag, 0)
+	for _, tag := range tags {
+		tagsByID[tag.ID] = tag
+	}
+
+	for _, reading := range readings {
+		for _, tagID := range tagsByReading[reading.ID] {
+			if reading.Tags == nil {
+				reading.Tags = make([]*tag, 0)
+			}
+
+			reading.Tags = append(reading.Tags, tagsByID[tagID])
+		}
+	}
+
+	return nil
 }
